401d0699e4e2b69dd90a0b50712d8ee72f6b309e
eaarroyavave <eaarroyave@softcaribbean.com.co>
2022-09-27 19:18:08 -0500

    Iniciamos configuracion del repositorio

---
+++ src/main/java/com/udea/fundamentos/ventaspormayor/SeguimientoDeVentasApplication.java
@@ -0,0 +1,13 @@
+package com.udea.fundamentos.ventaspormayor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeguimientoDeVentasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeguimientoDeVentasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/udea/fundamentos/ventaspormayor/SeguimientoDeVentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udea.fundamentos.ventaspormayor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeguimientoDeVentasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
